4c77f039397d4b43fd337916d598e61389d4663b
Arivalagan Mathiyalagu <arimathi123@gmail.com>
2025-02-10 14:26:10 +0000

    dashboard

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,7 +1,12 @@
 
 function Dashboard() {
   return (
-    <div>Stats</div>
+    <>
+      <div class="absolute top-4 left-4 p-4 bg-white shadow-md rounded-md w-48">
+        <div class="text-gray-500 text-sm">Total Patients</div>
+        <div class="text-4xl font-semibold text-gray-800">32</div>
+      </div>
+    </>
   )
 }
 
