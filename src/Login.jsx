bbfd3ee918fa17e3f458482c1800d5e298293a4f
rajavasanthan <rajavasanthan@gmail.com>
2025-02-12 16:50:28 +0530

    logo

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useFormik } from "formik";
+import { User } from "lucide-react";
 import { useNavigate } from "react-router";
 
 function Login() {
@@ -41,17 +42,14 @@ function Login() {
     <>
       <section className="bg-gray-50 dark:bg-gray-900">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <a
-            href="#"
-            className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-          >
-            <img
-              className="w-8 h-8 mr-2"
-              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-              alt="logo"
-            />
-            Doc Management
-          </a>
+        <div className="flex justify-center">
+        <div className="flex flex-col items-center space-y-4 pb-4">
+          <div className="w-24 h-24 bg-blue-500 rounded-full flex items-center justify-center">
+            <User className="w-12 h-12 text-white" />
+          </div>
+          <h1 className="text-2xl font-bold text-white">Dr.Dinesh</h1>
+        </div>
+        </div>
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
